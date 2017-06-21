98aa97f60df761a2350f26c2d46ffe18177c1390
Flavio Martins <flaviomartins@acm.org>
2017-06-21 22:36:52 +0100

    sync with CreateIndex

--- src/main/java/me/lemire/lucene/IndexDump.java
+++ src/main/java/me/lemire/lucene/IndexDump.java
@@ -27,6 +27,14 @@ public class IndexDump {
                         return;
                 }
                 File wikipediafile = new File(args[0]);
+                int commitInterval = 1000000;
+
+                if (args.length >= 2) {
+                        commitInterval = Integer.parseInt(args[1]);
+                }
+
+                System.out.println("Commiting after indexing " + commitInterval + " docs");
+
                 if (!wikipediafile.exists()) {
                         System.out.println("Can't find "
                                 + wikipediafile.getAbsolutePath());
@@ -40,23 +48,18 @@ public class IndexDump {
                 File outputDir = new File(args[1]);
                 if (!outputDir.exists()) {
                         if (!outputDir.mkdirs()) {
-                                System.out.println("couldn't create "
-                                        + outputDir.getAbsolutePath());
+                                System.out.println("couldn't create " + outputDir.getAbsolutePath());
                                 return;
                         }
                 }
                 if (!outputDir.isDirectory()) {
-                        System.out.println(outputDir.getAbsolutePath()
-                                + " is not a directory!");
+                        System.out.println(outputDir.getAbsolutePath() + " is not a directory!");
                         return;
                 }
                 if (!outputDir.canWrite()) {
-                        System.out.println("Can't write to "
-                                + outputDir.getAbsolutePath());
+                        System.out.println("Can't write to " + outputDir.getAbsolutePath());
                         return;
                 }
-
-                // we should be "ok" now
 
                 FSDirectory dir = FSDirectory.open(outputDir);
 
@@ -78,6 +81,11 @@ public class IndexDump {
                                                                            // document
                                                                            // only
                                                                            // once
+                properties.setProperty("doc.index.props", "true");
+                // We want to store small-size fields like URL or even title  ...
+                properties.setProperty("doc.stored", "true");
+                // but not the large one (great savings, 3x reduction in space)!
+                properties.setProperty("doc.body.stored", "false");
                 properties.setProperty("docs.file",
                         wikipediafile.getAbsolutePath());
                 properties.setProperty("keep.image.only.docs", "false");
@@ -96,17 +104,17 @@ public class IndexDump {
                         while ((doc = docMaker.makeDocument()) != null) {
                                 indexWriter.addDocument(doc);
                                 ++count;
-                                if (count % 1000 == 0)
-                                        System.out
-                                                .println("Indexed "
-                                                        + count
-                                                        + " documents in "
-                                                        + (System
-                                                                .currentTimeMillis() - start)
-                                                        + " ms");
+                                if (count % 5000 == 0) {
+                                        System.out.println("Indexed " + count + " documents in "
+                                                + (System.currentTimeMillis() - start) + " ms");
+                                }
+                                if (count % commitInterval == 0) {
+                                        indexWriter.commit();
+                                        System.out.println("Committed");
+                                }
                         }
                 } catch (org.apache.lucene.benchmark.byTask.feeds.NoMoreDataException nmd) {
-                        nmd.printStackTrace();
+                        System.out.println("Caught NoMoreDataException! -- Finishing"); // All done
                 }
                 long finish = System.currentTimeMillis();
                 System.out.println("Indexing " + count + " documents took "
@@ -116,6 +124,7 @@ public class IndexDump {
                 System.out.println("Index should be located at "
                         + dir.getDirectory().getAbsolutePath());
                 docMaker.close();
+                indexWriter.commit();
                 indexWriter.close();
         }
 
